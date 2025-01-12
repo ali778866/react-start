644e9041ec4efbf8b16e524e56ac8482d50c048c
ali778866 <mdali.ali76@gmail.com>
2025-01-12 20:24:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpenseItems from './components/ExpenseItem';
+
+function App(){
+  return (
+    <div>
+      <h2>Let's Get Started</h2>
+      <ExpenseItems />
+    </div>
+  )
+}
+export default App;
