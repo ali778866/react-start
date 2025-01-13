0e65765e3b1b8f6cbe45c79f5920b4f340664e1b
ali778866 <mdali.ali76@gmail.com>
2025-01-13 11:12:57 +0530

    Complex JSX and css Added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+    return (
+        <div className="expense-item">
+            <div>March 10th 2025</div>
+            <div className="expense-item_description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item_price">$205.99</div>
+            </div>
+        </div>
+    )
 }
 export default ExpenseItem; 
